f38b59a5eecd8443a393b4438f8bdab34c9be79c
linglinzhang <linglinca@gmail.com>
2019-07-29 00:00:41 +0200

    question 54

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.UnaryOperator;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
+
     public static void main(String[] args) {
-        List<Double> codes = Arrays.asList(10.0, 20.0);
-        UnaryOperator<Double> uo = s -> s + 10;
-        codes.replaceAll(uo);
-        codes.forEach(c -> System.out.println(c));
+        DoubleStream str= DoubleStream.of(1.0,2.0,3.0,4.0);
+        Double d = str.average().getAsDouble();
+        System.out.println(d);
     }
 }
