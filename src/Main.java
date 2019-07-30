33cbe46b581717a5c442476d988064cab1bdffc3
linglinzhang <linglinca@gmail.com>
2019-07-30 23:39:15 +0200

    Q 150

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.function.Predicate;
+import java.util.function.Function;
+import java.util.function.IntConsumer;
+import java.util.function.IntFunction;
+import java.util.function.ToIntFunction;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Product> products = new ArrayList<> (Arrays.asList(new Product(1,10), new Product(2,30), new Product(2,30)));
-        Product p = products.stream().reduce(new Product(4,0), (p1,p2) -> {
-            p1.price+=p2.price;
-            return new Product(p1.id, p1.price);
-        });
-        products.add(p);
-        products.stream().parallel().reduce((p1,p2) -> p1.price>p2.price?p1:p2)
-                .ifPresent(System.out::print);
+        IntConsumer consumer = e-> System.out.println(e);
+        Integer value =90;
+        ToIntFunction<Integer> funRef = e -> e + 10;
+        Integer result =  funRef.applyAsInt(value);
+        consumer.accept(result);
     }
 }
