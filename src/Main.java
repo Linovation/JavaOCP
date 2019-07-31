87996a5761758c2c164fa7a06bb5adb3f485f867
linglinzhang <linglinca@gmail.com>
2019-07-31 12:42:33 +0200

    Q addPop

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
 import java.util.function.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> words = Arrays.asList("win","try","best","luck","do");
-        Predicate<String> test1 = w-> {
-            System.out.println("Checking");
-            return w.equals("do");
-        };
-        Predicate<String> test2 = w -> w.length() > 3;
-        words.stream().filter(test2).filter(test1).count();
+        Deque<Integer> nums = new ArrayDeque<>();
+        nums.add(1000);
+        nums.push(2000);
+        nums.add(3000);
+        nums.push(4000);
+        Integer i1 = nums.remove();
+        Integer i2 = nums.pop();
+        System.out.println(i1 + ":" + i2);
     }
 }
